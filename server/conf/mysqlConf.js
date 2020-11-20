c2e8390d52a0554fd457730d85af7339c0687784
CodeFly16 <466885667@qq.com>
2020-11-20 12:35:03 +0800

    部署

--- server/conf/mysqlConf.js
+++ server/conf/mysqlConf.js
@@ -1,7 +1,8 @@
 module.exports = {
   host: '47.98.225.213',
   user: 'root',
-  password: '123456',
+  // password: '123456',
+  password: 'Wk19961220',
   port: '3306',
   database: 'booksdb',
   // 最大连接数，默认为10
